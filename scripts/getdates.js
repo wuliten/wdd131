e4fed55352e66e82a0d06958a4d98862981f0c32
wuliten <bryandborst@gmail.com>
2026-03-12 15:17:07 -0400

    1

---
+++ scripts/getdates.js
@@ -0,0 +1,6 @@
+const currentYear = new Date().getFullYear();
+
+document.getElementById("currentyear").textContent = currentYear;
+
+document.getElementById("lastModified").textContent =
+    "Last Modified: " + document.lastModified;
