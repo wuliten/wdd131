7d730a13b8bc0ae1f8bfea7a0095168efd95b2e7
wuliten <bryandborst@gmail.com>
2026-03-27 19:25:50 -0400

    3

--- scripts/place.js
+++ scripts/place.js
@@ -1,18 +1,18 @@
 document.getElementById("year").textContent = new Date().getFullYear();
 document.getElementById("lastModified").textContent = document.lastModified;
 
-const temperature = 48; 
-const windSpeed = 10;  
+const temperature = 10;
+const windSpeed = 16;
 
 function calculateWindChill(temp, speed) {
-    return 35.74 + (0.6215 * temp) - (35.75 * Math.pow(speed, 0.16)) + (0.4275 * temp * Math.pow(speed, 0.16));
+    return 13.12 + (0.6215 * temp) - (11.37 * Math.pow(speed, 0.16)) + (0.3965 * temp * Math.pow(speed, 0.16));
 }
 
 const windChillDisplay = document.getElementById("windChill");
 
-if (temperature <= 50 && windSpeed > 3) {
+if (temperature <= 10 && windSpeed > 4.8) {
     const chill = calculateWindChill(temperature, windSpeed);
-    windChillDisplay.textContent = chill.toFixed(1) + " °F";
+    windChillDisplay.textContent = chill.toFixed(1) + " °C";
 } else {
     windChillDisplay.textContent = "N/A";
 }
